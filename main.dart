ae95217f66744751dfd5cd370be24e8a2843d38a
adamdahou <dahoumouhamedhamza@gmail.com>
2025-05-24 17:38:35 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'عرش الممالك',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('عرش الممالك - ADAMAPPS')),
+      body: const Center(child: Text('مرحباً بك في لعبة عرش الممالك!')),
+    );
+  }
+}
